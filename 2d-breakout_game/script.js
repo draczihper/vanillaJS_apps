b5bf52fd9012f1db61693039b30d00b0043dde72
Drac Zihper <draczihper@gmail.com>
2024-05-03 13:27:37 +0300

    Add function to draw and move user

--- 2d-breakout_game/script.js
+++ 2d-breakout_game/script.js
@@ -1,7 +1,106 @@
 const grid = document.querySelector(".grid");
+const blockWidth = 100;
+const blockHeight = 20;
+const boardWidth = 560;
+const ballDiameter = 20;
+let timerId;
+
+const userStart = [230, 10];
+let currentPosition = userStart;
+
+const ballStart = [270, 30];
+let ballCurrentPosition = ballStart
+
+class Block {
+    constructor(xAxis, yAxis) {
+        this.bottomLeft = [xAxis, yAxis];
+        this.bottomRight = [xAxis + blockWidth, yAxis];
+        this.topLeft = [xAxis, yAxis + blockHeight];
+        this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
+    }
+}
+
+const blocks = [
+    new Block(10, 270),
+    new Block(120, 270),
+    new Block(230, 270),
+    new Block(340, 270),
+    new Block(450, 270),
+    new Block(10, 240),
+    new Block(120, 240),
+    new Block(230, 240),
+    new Block(340, 240),
+    new Block(450, 240),
+    new Block(10, 210),
+    new Block(120, 210),
+    new Block(230, 210),
+    new Block(340, 210),
+    new Block(450, 210),
+];
 
 
+function addBlocks() {
+  for (let i = 0; i < blocks.length; i++){
+    const block = document.createElement("div");
+    block.classList.add("block");
+    block.style.left = blocks[i].bottomLeft[0] + 'px';
+    block.style.bottom = blocks[i].bottomLeft[1] + 'px';
+    grid.appendChild(block);
+  }
+}
 
-const block = document.createElement('div');
-block.classList.add('block');
-grid.appendChild(block);
+addBlocks();
+
+const user = document.createElement('div');
+user.classList.add('user');
+drawUser();
+grid.appendChild(user);
+
+function drawUser() {
+    user.style.left = currentPosition[0] + 'px';
+    user.style.bottom = currentPosition[1] + 'px';
+}
+
+function drawBall() {
+    ball.style.left = ballCurrentPosition[0] + 'px';
+ball.style.bottom = ballCurrentPosition[1] + 'px';
+}
+
+function moveUser (e) {
+    switch(e.key) {
+        case 'ArrowLeft':
+            if (currentPosition[0] > 0) {
+                currentPosition[0] -= 10;
+                drawUser();
+            }
+            break;
+        case 'ArrowRight':
+            if(currentPosition[0] < boardWidth - blockWidth) {
+                currentPosition[0] += 10;
+                drawUser();
+            }
+            break;
+    }
+}
+
+document.addEventListener('keydown', moveUser);
+
+const ball = document.createElement('div');
+ball.classList.add('ball');
+drawBall();
+grid.appendChild(ball);
+
+function moveBall() {
+    ballCurrentPosition[0] += 2;
+    ballCurrentPosition[1] += 2;
+    drawBall();
+}
+
+timerId = setInterval(moveBall, 30);
+
+
+function checkForCollisions() {
+    if (currentPosition[0] >= (boardWidth - ballDiameter)) {
+        
+    }
+}
