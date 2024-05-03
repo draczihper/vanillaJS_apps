720f1add8c4586f7c36cd5657aff7933d9fe81ae
Drac Zihper <draczihper@gmail.com>
2024-05-03 14:37:25 +0300

    Add loop to check block collisions

--- 2d-breakout_game/script.js
+++ 2d-breakout_game/script.js
@@ -8,6 +8,7 @@ const ballDiameter = 20;
 let timerId;
 let xDirection = -2;
 let yDirection = 2;
+let score = 0;
 
 const userStart = [230, 10];
 let currentPosition = userStart;
@@ -105,6 +106,20 @@ timerId = setInterval(moveBall, 30);
 
 
 function checkForCollisions() {
+    for (let i = 0; i < blocks.length; i++) {
+       if (
+        (ballCurrentPosition[0] > blocks[i].bottomLeft[0] && ballCurrentPosition[0] < blocks[i].bottomRight[0] && ((ballCurrentPosition[1] + ballDiameter) > blocks[i].bottomLeft[1] && ballCurrentPosition[1] < blocks[i].topLeft[1]))
+       ) {
+        const allBlocks = Array.from(document.querySelectorAll('.block'));
+        allBlocks[i].classList.remove('block');
+        blocks.splice(i, 1);
+        changeDirection();
+        score++;
+        scoreDisplay.innerHTML = score;
+       }
+    }
+
+
     if (ballCurrentPosition[0] >= (boardWidth - ballDiameter) || 
     ballCurrentPosition[1] >= (boardHeight - ballDiameter)||
     ballCurrentPosition[0] <= 0
