c615e873f302ba43ebddfea98fe284a25781cc7d
Drac Zihper <draczihper@gmail.com>
2024-05-03 14:18:31 +0300

    Add function to check for collisions and change direction

--- 2d-breakout_game/script.js
+++ 2d-breakout_game/script.js
@@ -1,9 +1,13 @@
 const grid = document.querySelector(".grid");
+const scoreDisplay = document.querySelector('#score');
 const blockWidth = 100;
 const blockHeight = 20;
 const boardWidth = 560;
+const boardHeight = 300;
 const ballDiameter = 20;
 let timerId;
+let xDirection = -2;
+let yDirection = 2;
 
 const userStart = [230, 10];
 let currentPosition = userStart;
@@ -91,16 +95,48 @@ drawBall();
 grid.appendChild(ball);
 
 function moveBall() {
-    ballCurrentPosition[0] += 2;
-    ballCurrentPosition[1] += 2;
+    ballCurrentPosition[0] += xDirection;
+    ballCurrentPosition[1] += yDirection;
     drawBall();
+    checkForCollisions();
 }
 
 timerId = setInterval(moveBall, 30);
 
 
 function checkForCollisions() {
-    if (currentPosition[0] >= (boardWidth - ballDiameter)) {
-        
+    if (ballCurrentPosition[0] >= (boardWidth - ballDiameter) || 
+    ballCurrentPosition[1] >= (boardHeight - ballDiameter)||
+    ballCurrentPosition[0] <= 0
+) {
+        changeDirection();
+    }
+
+    if (ballCurrentPosition[1] <= 0) {
+        clearInterval(timerId);
+        scoreDisplay.innerHTML = 'You lose';
+        document.removeEventListener('keydown', moveUser)
+    }
+}
+
+function changeDirection() {
+    if(xDirection === 2 && yDirection === 2) {
+        yDirection = -2;
+        return;
+    }
+
+    if(xDirection === 2 && yDirection === -2) {
+        xDirection = -2;
+        return;
+    }
+
+    if (xDirection === -2 && yDirection === -2) {
+        yDirection = 2;
+        return
+    }
+
+    if (xDirection === -2 && yDirection === 2) {
+        xDirection = 2;
+        return;
     }
 }
