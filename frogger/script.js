0bb2de1efb2fa6a49603b41518640df3a55466d6
Drac Zihper <draczihper@gmail.com>
2024-05-14 16:57:47 +0300

    Add DOM selectors

---
+++ frogger/script.js
@@ -0,0 +1,8 @@
+const timeLeftDisplay = document.querySelector('#time-left');
+const resultDisplay =  document.querySelector('#result');
+const startPauseButton = document.querySelector('#start-pause-button');
+const squares = document.querySelectorAll('.grid div');
+
+function moveFrog() {
+    
+}
