958bcfdcde4dc7024aa5a032cdc041b1e253a478
Drac Zihper <draczihper@gmail.com>
2024-05-15 19:55:49 +0300

    Add moving frog feature

--- frogger/script.js
+++ frogger/script.js
@@ -1,27 +1,28 @@
-const timeLeftDisplay = document.querySelector('#time-left');
-const resultDisplay =  document.querySelector('#result');
-const startPauseButton = document.querySelector('#start-pause-button');
-const squares = document.querySelectorAll('.grid div');
+const timeLeftDisplay = document.querySelector("#time-left");
+const resultDisplay = document.querySelector("#result");
+const startPauseButton = document.querySelector("#start-pause-button");
+const squares = document.querySelectorAll(".grid div");
 let currentIndex = 76;
-
+const width = 9;
 
 function moveFrog(e) {
-    
-    switch (e.key) {
-        case 'ArrowLeft':
-            console.log("move left");
-            break;
-        case 'ArrowRight':
-            console.log("move right");
-            break;
-        case "ArrowUp":
-            console.log("move up");
-            break;
-        case 'ArrowDown':
-            console.log("move down");
-            break;
-    }
-    squares[currentIndex].classList.add('frog');
+  squares[currentIndex].classList.remove("frog");
+
+  switch (e.key) {
+    case "ArrowLeft":
+      currentIndex -= 1;
+      break;
+    case "ArrowRight":
+      currentIndex += 1;
+      break;
+    case "ArrowUp":
+      currentIndex -= width;
+      break;
+    case "ArrowDown":
+      currentIndex += width;
+      break;
+  }
+  squares[currentIndex].classList.add("frog");
 }
 
-document.addEventListener('keyup', moveFrog);
+document.addEventListener("keyup", moveFrog);
