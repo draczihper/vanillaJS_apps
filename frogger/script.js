f223b025fd9ceb5a92a0d705ca30b87377e455a2
Drac Zihper <draczihper@gmail.com>
2024-05-17 11:25:52 +0300

    Add moveLogRight func

--- frogger/script.js
+++ frogger/script.js
@@ -2,7 +2,8 @@ const timeLeftDisplay = document.querySelector("#time-left");
 const resultDisplay = document.querySelector("#result");
 const startPauseButton = document.querySelector("#start-pause-button");
 const squares = document.querySelectorAll(".grid div");
-const logsLeft = document.querySelectorAll('.log-left')
+const logsLeft = document.querySelectorAll(".log-left");
+const logsRight = document.querySelectorAll(".log-right");
 let currentIndex = 76;
 const width = 9;
 
@@ -11,13 +12,13 @@ function moveFrog(e) {
 
   switch (e.key) {
     case "ArrowLeft":
-      if (currentIndex % width !== 0)currentIndex -= 1;
+      if (currentIndex % width !== 0) currentIndex -= 1;
       break;
     case "ArrowRight":
       if (currentIndex % width < width - 1) currentIndex += 1;
       break;
     case "ArrowUp":
-      if (currentIndex - width >= 0 )currentIndex -= width;
+      if (currentIndex - width >= 0) currentIndex -= width;
       break;
     case "ArrowDown":
       if (currentIndex + width < width * width) currentIndex += width;
@@ -29,27 +30,58 @@ function moveFrog(e) {
 document.addEventListener("keyup", moveFrog);
 
 function autoMoveLogs() {
-  logsLeft.forEach(logLeft => moveLogLeft(logLeft))
+  logsLeft.forEach((logLeft) => moveLogLeft(logLeft));
+  logsRight.forEach((logRight) => moveLogRight(logRight));
 }
 
 function moveLogLeft(logLeft) {
   switch (true) {
-    case logLeft.classList.contains('l1'):
-      logLeft.classList.remove('l1');
-      logLeft.classList.add('l2');
+    case logLeft.classList.contains("l1"):
+      logLeft.classList.remove("l1");
+      logLeft.classList.add("l2");
       break;
-    case logLeft.classList.contains('l2'):
-        logLeft.classList.remove('l2');
-        logLeft.classList.add('l3');
+    case logLeft.classList.contains("l2"):
+      logLeft.classList.remove("l2");
+      logLeft.classList.add("l3");
       break;
-      case logLeft.classList.contains('l3'):
-        logLeft.classList.remove('l3');
-        logLeft.classList.add('l4');
-        break;
-      
-  
-    
+    case logLeft.classList.contains("l3"):
+      logLeft.classList.remove("l3");
+      logLeft.classList.add("l4");
+      break;
+    case logLeft.classList.contains("l4"):
+      logLeft.classList.remove("l4");
+      logLeft.classList.add("l5");
+      break;
+    case logLeft.classList.contains("l5"):
+      logLeft.classList.remove("l5");
+      logLeft.classList.add("l1");
+      break;
   }
 }
 
-setInterval(autoMoveLogs, 300);
+function moveLogRight(logRight) {
+  switch (true) {
+    case logRight.classList.contains("l1"):
+      logRight.classList.remove("l1");
+      logRight.classList.add("l5");
+      break;
+    case logRight.classList.contains("l2"):
+      logRight.classList.remove("l2");
+      logRight.classList.add("l1");
+      break;
+    case logRight.classList.contains("l3"):
+      logRight.classList.remove("l3");
+      logRight.classList.add("l2");
+      break;
+    case logRight.classList.contains("l4"):
+      logRight.classList.remove("l4");
+      logRight.classList.add("l3");
+      break;
+    case logRight.classList.contains("l5"):
+      logRight.classList.remove("l5");
+      logRight.classList.add("l4");
+      break;
+  }
+}
+
+setInterval(autoMoveLogs, 1000);
