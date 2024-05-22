fe9db143b452e98fbd171635239e8c02cad4a9ea
Drac Zihper <draczihper@gmail.com>
2024-05-22 11:51:00 +0300

    Add DOM selectors

--- connect-four/script.js
+++ connect-four/script.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelector('.grid div');
+    const result = document.querySelector('#result');
+    const displayCurrentPlayer = document.querySelector('#current-player');
+
+    let currentPlayer = 1;
     
-})
+});
