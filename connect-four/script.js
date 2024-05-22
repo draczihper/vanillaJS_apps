3d8aa985f87c2d4767b078209201d76e5a9458d7
Drac Zihper <draczihper@gmail.com>
2024-05-22 12:17:49 +0300

    Add player switch

--- connect-four/script.js
+++ connect-four/script.js
@@ -7,7 +7,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     for(let i = 0; i < squares.length; i++) {
         squares[i].onclick = () => {
-            
+            if (squares[i + 7].classList.contains('taken')) {
+                if (currentPlayer == 1) {
+                    squares[i].classList.add('taken');
+                    squares[i].classList.add('player-one');
+                    currentPlayer = 2;
+                    displayCurrentPlayer.innerHTML = currentPlayer;
+                }
+            }
         }
     }
 });
